f7a632e543e6bf8f899803d7f57009310e23da3d
Öznur Demir <dmroznurr@gmail.com>
2024-06-26 19:30:00 +0300

    Completed Splash Screen.

---
+++ app/src/main/java/com/example/composenewsapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.composenewsapp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.composenewsapp.ui.theme.ComposeNewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            ComposeNewsAppTheme {
+
+            }
+        }
+    }
+}
